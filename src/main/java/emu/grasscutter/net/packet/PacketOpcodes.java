0596c9e465ce0c1fe7210e7d4f03529d1be76f49
yunaochan <2640910163@qq.com>
2024-09-09 16:10:16 +0800

    1

--- src/main/java/emu/grasscutter/net/packet/PacketOpcodes.java
+++ src/main/java/emu/grasscutter/net/packet/PacketOpcodes.java
@@ -545,7 +545,7 @@ public final class PacketOpcodes {
 	public static final int BuyBattlePassLevelRsp = 7500;//26350,7408,23606,7500,3113,5074,28349,7072
 	public static final int SetBattlePassViewedReq = 26483;
 	// or 22048,533,26489,902,28919,9890,23101,21184,9036
-	public static final int SetBattlePassViewedRsp = 533;// or ,26489,902,23101,21184,9036
+	public static final int SetBattlePassViewedRsp = 26489;// or 902,23101,21184,9036
 	public static final int TakeBattlePassMissionPointReq = 6231;
 	public static final int TakeBattlePassRewardReq = 29828;
 	public static final int TakeBattlePassRewardRsp = 941;
