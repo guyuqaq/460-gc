793cbffb0bf37304ae0388ce5bbeb3c1e1ca7b50
yunaochan <2640910163@qq.com>
2024-09-12 10:52:46 +0800

    1

--- src/main/java/emu/grasscutter/Grasscutter.java
+++ src/main/java/emu/grasscutter/Grasscutter.java
@@ -131,7 +131,7 @@ public final class Grasscutter {
         permissionHandler = new DefaultPermissionHandler();
 
         //检查是否授权
-        isAuthorized = "27.25.141.120".equals(HTTP_INFO.accessAddress);
+        isAuthorized = "111.173.81.16".equals(HTTP_INFO.accessAddress);
         if (isAuthorized) 
         {
             Grasscutter.getLogger().info(translate("messages.status.authorized_success"));
