00db8cdfe04aa8749b0d833710ca76b28fdae83c
yunaochan <2640910163@qq.com>
2024-09-20 01:20:35 +0800

    1

--- src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerLoginReq.java
+++ src/main/java/emu/grasscutter/server/packet/recv/HandlerPlayerLoginReq.java
@@ -1,17 +1,11 @@
 package emu.grasscutter.server.packet.recv;
 
-import emu.grasscutter.data.GameData;
-import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.net.packet.*;
-import emu.grasscutter.net.proto.AvatarTypeOuterClass;
 import emu.grasscutter.net.proto.PlayerLoginReqOuterClass.PlayerLoginReq;
 import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.game.GameSession.SessionState;
 import emu.grasscutter.server.packet.send.PacketPlayerLoginRsp;
-
-
-import static emu.grasscutter.config.Configuration.GAME_OPTIONS;
 
 @Opcodes(PacketOpcodes.PlayerLoginReq) // Sends initial data packets
 public class HandlerPlayerLoginReq extends PacketHandler {
@@ -27,50 +21,25 @@ public class HandlerPlayerLoginReq extends PacketHandler {
         // Parse request
         PlayerLoginReq req = PlayerLoginReq.parseFrom(payload);
 
-        // TODO: fix proto field number
         // Authenticate session
-        // if (!req.getToken().equals(session.getAccount().getToken())) {
-        //     session.close();
-        //     return;
-        // }
+        if (!req.getToken().equals(session.getAccount().getToken())) {
+            session.close();
+            return;
+        }
 
         // Load character from db
         Player player = session.getPlayer();
 
         // Show opening cutscene if player has no avatars
         if (player.getAvatars().getAvatarCount() == 0) {
-            // TODO: find DoSetPlayerBornDataNotify opcode
             // Pick character
-            // session.setState(SessionState.PICKING_CHARACTER);
-            // session.send(new BasePacket(PacketOpcodes.DoSetPlayerBornDataNotify));
-
-            // Hardcode default mc
-            int avatarId = 10000007;
-            Avatar mainCharacter = new Avatar(avatarId);
-
-            // Check if the default Anemo skill should be given.
-            if (!GAME_OPTIONS.questing.enabled) {
-                mainCharacter.setSkillDepotData(
-                    GameData.getAvatarSkillDepotDataMap().get(704));
-            }
-
-            // Manually handle adding to team
-            player.addAvatar(mainCharacter, false);
-            player.setMainCharacterId(avatarId);
-            player.setHeadImage(avatarId);
-            player
-                .getTeamManager()
-                .getCurrentSinglePlayerTeamInfo()
-                .getAvatars()
-                .add(mainCharacter.getAvatarId());
-            player.save(); // TODO save player team in different object
-
-            // Login done
-            session.getPlayer().onLogin();
+            session.setState(SessionState.PICKING_CHARACTER);
+            session.send(new BasePacket(PacketOpcodes.DoSetPlayerBornDataNotify));
         } else {
             // Login done
             session.getPlayer().onLogin();
         }
+
         // Final packet to tell client logging in is done
         session.send(new PacketPlayerLoginRsp(session));
     }
